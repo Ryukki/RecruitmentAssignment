359342ea88979c4b88068d9d429a19407746fdaa
Ryukki <jakubwidlak@wp.pl>
2017-11-14 17:17:48 +0100

    Final version.

--- assignment/src/main/java/com/jakubwidlak/assignment/AssignmentApplication.java
+++ assignment/src/main/java/com/jakubwidlak/assignment/AssignmentApplication.java
@@ -1,18 +1,12 @@
 package com.jakubwidlak.assignment;
 
-import com.jakubwidlak.assignment.dataprovider.HttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
-		/*HttpClient httpClient = new HttpClient();
-		List<String> userRepos = httpClient.getUserRepos();
-		httpClient.languageStatistics(userRepos);*/
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
 }
